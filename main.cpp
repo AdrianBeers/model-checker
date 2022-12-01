aad6535a7cf94f3fa41476fd0d24c231b972a9f3
Jens Coenders <jens@famcoenders.nl>
2022-12-01 12:19:57 +0100

    Add loading of input files from command line

--- main.cpp
+++ main.cpp
@@ -3,21 +3,61 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
-int main() {
-    cout << "Enter mu-calculus formula:" << endl;
+bool readFileContents(const char* fileName, string &out) {
+    // Open file stream
+    ifstream in;
+    in.open(fileName);
 
-    // Example input: (([action]true && mu X.[p]X) || Y)
-    string input;
-    getline(cin, input);
+    // Check if file exists
+    if (!in.is_open()) {
+        return false;
+    }
 
-    // Parse string to formula
+    // Read file contents
+    stringstream stream;
+    stream << in.rdbuf();
+    out = stream.str();
+
+    // Close input stream
+    in.close();
+
+    return true;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        cout << "Usage: model_checker <mu_calculus_file> <lts_file>" << endl;
+        return 1;
+    }
+
+    // Read input files
+    string formulaInput, ltsInput;
+    if (!readFileContents(argv[1], formulaInput)) {
+        cout << "Could not read mu calculus input file" << endl;
+        return 1;
+    }
+    if (!readFileContents(argv[2], ltsInput)) {
+        cout << "Could not read lts input file" << endl;
+        return 1;
+    }
+
+    // Take first line of formula input file as mu-calculus formula input
+    size_t eol = formulaInput.find('\n');
+    if (eol != string::npos) {
+        formulaInput = formulaInput.substr(0, eol);
+    }
+
+    // Parse mu-calculus formula
     MuCalculusParser p;
-    shared_ptr<Formula> f = p.parse(input);
+    shared_ptr<Formula> f = p.parse(formulaInput);
 
     // Print formula to string
+    cout << "Parsed mu-calculus formula:" << endl;
     f->pprint();
     cout << endl;
 
